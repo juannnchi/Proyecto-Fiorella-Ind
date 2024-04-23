00c7cc309a90e7df750acd08508ffb7df19e709b
Juan Cruz Montagni <juanchi.montagni@gmail.com>
2024-04-23 18:38:18 -0300

    Initial commit

---
+++ productos.js
@@ -0,0 +1,32 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "xxxx",
+        precio: 1000,
+        img: "https://lavaindumentaria.com.ar/wp-content/uploads/2022/08/ccc.jpg",
+    },
+    {
+        id: 2,
+        nombre: "xxxx",
+        precio: 2000,
+        img: "https://lavaindumentaria.com.ar/wp-content/uploads/2022/08/ccc.jpg",
+    },
+    {
+        id: 3,
+        nombre: "xxxx",
+        precio: 3000,
+        img: "https://lavaindumentaria.com.ar/wp-content/uploads/2022/08/ccc.jpg",
+    },
+    {
+        id: 4,
+        nombre: "xxxx",
+        precio: 4000,
+        img: "https://lavaindumentaria.com.ar/wp-content/uploads/2022/08/ccc.jpg",
+    },
+    {
+        id: 5,
+        nombre: "xxxx",
+        precio: 5000,
+        img: "https://lavaindumentaria.com.ar/wp-content/uploads/2022/08/ccc.jpg",
+    }
+]
