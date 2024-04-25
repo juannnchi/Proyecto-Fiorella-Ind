b2513a9767445b1ed5a7e0d35ecf21de13848f25
juan cruz montagni <juanchi.montagni@gmail.com>
2024-04-25 14:23:02 -0300

    agregamos modal
    corregimos verCarrito, no se acumula y se resetea el innerHTML del container.
    Trabajamos en la transicion del modal

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ productos.forEach((product) => {
 });
 
 verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "block";
+    modalContainer.classList.add("modal-container-active");
     
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
@@ -45,7 +48,32 @@ verCarrito.addEventListener("click", () => {
     modalBtn.innerHTML = `
         <i class="bi bi-x"></i>
     `;
+
+    modalBtn.addEventListener("click", () => {
+        modalContainer.classList.remove("modal-container-active");
+        modalContainer.style.display = "none";
+    })
     
-    modalBtn.className = "modal-close";
-    modalContainer.append(modalBtn);
+    modalBtn.className = "modal-header-btn";
+    modalHeader.append(modalBtn);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = ` 
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <span>$${product.precio}</span>
+        `;
+
+        modalContainer.append(carritoContent);
+    });
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuying = document.createElement("div");
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `Total a pagar: $${total}.`;
+
+    modalContainer.append(totalBuying);
 });
