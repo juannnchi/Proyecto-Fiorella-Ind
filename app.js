05dc30df984f1b920ea8a2983130077f9d3d593e
Juan Cruz Montagni <juanchi.montagni@gmail.com>
2024-04-24 23:54:40 -0300

    creando modal
    para ver el carrito

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modalContainer");
 
 let carrito = [];
 
@@ -6,12 +8,44 @@ productos.forEach((product) => {
     let content = document.createElement("div");
     content.className = "card"
     content.innerHTML = `
-        <img src="${product.img}">
-        <h3 class="card-title">${product.nombre}</h3>
-        <span class="card-price">$${product.precio}</span>
-        <button class="card-btn">Agregar al carrito</button>
+    <img src="${product.img}">
+    <h3 class="card-title">${product.nombre}</h3>
+    <span class="card-price">$${product.precio}</span>
     `;
-
+    
     shopContent.append(content);
+    let comprar = document.createElement("button");
+    comprar.innerText = "Agregar al carrito";
+    comprar.className = "card-btn";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click",() => {
+        carrito.push({
+            id: product.id,
+            nombre: product.nombre,
+            img: product.img,
+            precio: product.precio,
+        });
+        console.log(carrito);
+    });
 });
 
+verCarrito.addEventListener("click", () => {
+    
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+        <h1 class="modal-title">Carrito</h1>
+    `;
+
+    modalContainer.append(modalHeader);
+    
+    const modalBtn = document.createElement("button");
+    modalBtn.innerHTML = `
+        <i class="bi bi-x"></i>
+    `;
+    
+    modalBtn.className = "modal-close";
+    modalContainer.append(modalBtn);
+});
