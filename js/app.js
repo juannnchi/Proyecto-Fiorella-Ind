7c9bc69b73268a7b9b49d80388d1e541b94a8cde
Juan Cruz Montagni <juanchi.montagni@gmail.com>
2024-04-25 23:41:31 -0300

    agregamos carpeta js

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modalContainer");
+
+let carrito = [];
+
+productos.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card"
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3 class="card-title">${product.nombre}</h3>
+    <span class="card-price">$${product.precio}</span>
+    `;
+    
+    shopContent.append(content);
+    let comprar = document.createElement("button");
+    comprar.innerText = "Agregar al carrito";
+    comprar.className = "card-btn";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click",() => {
+        carrito.push({
+            id: product.id,
+            nombre: product.nombre,
+            img: product.img,
+            precio: product.precio,
+        });
+        console.log(carrito);
+    });
+});
+
+
--- js/carrito.js
+++ js/carrito.js
@@ -1,37 +1,5 @@
-const shopContent = document.getElementById("shopContent");
-const verCarrito = document.getElementById("verCarrito");
-const modalContainer = document.getElementById("modalContainer");
+const pintarCarrito = () =>{
 
-let carrito = [];
-
-productos.forEach((product) => {
-    let content = document.createElement("div");
-    content.className = "card"
-    content.innerHTML = `
-    <img src="${product.img}">
-    <h3 class="card-title">${product.nombre}</h3>
-    <span class="card-price">$${product.precio}</span>
-    `;
-    
-    shopContent.append(content);
-    let comprar = document.createElement("button");
-    comprar.innerText = "Agregar al carrito";
-    comprar.className = "card-btn";
-
-    content.append(comprar);
-
-    comprar.addEventListener("click",() => {
-        carrito.push({
-            id: product.id,
-            nombre: product.nombre,
-            img: product.img,
-            precio: product.precio,
-        });
-        console.log(carrito);
-    });
-});
-
-verCarrito.addEventListener("click", () => {
     modalContainer.innerHTML = "";
     modalContainer.style.display = "block";
     modalContainer.classList.add("modal-container-active");
@@ -39,25 +7,24 @@ verCarrito.addEventListener("click", () => {
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
     modalHeader.innerHTML = `
-        <h1 class="modal-title">Carrito</h1>
+    <h1 class="modal-title">Carrito</h1>
     `;
-
+    
     modalContainer.append(modalHeader);
     
     const modalBtn = document.createElement("button");
     modalBtn.innerHTML = `
-        <i class="bi bi-x"></i>
+    <i class="bi bi-x"></i>
     `;
-
+    
     modalBtn.addEventListener("click", () => {
         modalContainer.classList.remove("modal-container-active");
         modalContainer.style.display = "none";
     })
     
-<<<<<<< Updated upstream
     modalBtn.className = "modal-header-btn";
     modalHeader.append(modalBtn);
-
+    
     carrito.forEach((product) => {
         let carritoContent = document.createElement("div");
         carritoContent.className = "modal-content";
@@ -66,22 +33,22 @@ verCarrito.addEventListener("click", () => {
         <h3>${product.nombre}</h3>
         <span>$${product.precio}</span>
         `;
-
+        
         modalContainer.append(carritoContent);
     });
-
+    
     const total = carrito.reduce((acc, el) => acc + el.precio, 0);
-
+    
     const totalBuying = document.createElement("div");
     totalBuying.className = "total-content"
     totalBuying.innerHTML = `Total a pagar: $${total}.`;
-
+    
     modalContainer.append(totalBuying);
-=======
     modalBtn.className = "modal-close";
     
     modalContainer.append(modalBtn);
+    
+    let carritoContent = document.createElement("div");  
+};
 
-    let carritoContent = document.createElement("div");
->>>>>>> Stashed changes
-});
+verCarrito.addEventListener("click", pintarCarrito);
